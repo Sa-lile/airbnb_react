17f1815955f1b6fdabd82272ce56398adaf6924c
Sachiyo <sachiyo.sable@gmail.com>
2024-10-09 18:23:07 +0200

    spread objecy as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
-import './App.css'
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
-import Card from './components/Card'
-
+import "./App.css";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
+import data from "./data";
+console.log(data); /* test data */
 
 function App() {
+
+ 
+  const cards = data.map((item) => {
+    // console.log(item.openSpots)
+
+    return (
+      <Card
+        key={item.id}
+        {...item}
+        /** all data */
+        // item={item}
+        // img={item.coverImg}
+        // rating={item.stats.rating}
+        // reviewCount={item.stats.reviewCount}
+        // country={item.location}
+        // title={item.title}
+        // price={item.price}
+        // openSpots= {item.openSpots}
+      />
+    );
+  });
 
   return (
     <>
       <Navbar />
       <Hero />
-      <Card />
+      <section className="cards-list">{cards}</section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,38 @@
-function Card() {
+
+
+function Card(props) {
+  // console.log(props);
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
+
   return (
     <>
       <div className="card">
+        {badgeText && <div className="card--badge">SOLD OUT</div>}
         <img
           className="card--image"
-          src="../src/assets/swimmer.png"
+          src={`../src/assets/${props.coverImg}`}
           alt="card--imag"
         />
-        <img
-          className="star-icon"
-          src="../src/assets/star-icon.png"
-          alt="star-icon"
-        />
-        <span>5.0</span>
-        <span>(6) • </span>
-        <span>USA</span>
-        <div>Life lessons with Katie Zaferes</div>
-        <div>
-          <span>From $136 </span>/ person
+        <div className="card--stats">
+          <img
+            className="star-icon"
+            src="../src/assets/star-icon.png"
+            alt="star-icon"
+          />
+          <span>{props.stats.rating}</span>
+          <span className="gray">({props.stats.reviewCount}) • </span>
+          <span className="gray">{props.location}</span>
         </div>
+        <p className="card--title">{props.title}</p>
+        <p className="card--price">
+          <span className="bold">From ${props.price}</span> / person
+        </p>
       </div>
     </>
   );
